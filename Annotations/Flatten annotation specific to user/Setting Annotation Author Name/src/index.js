dfc8a4d7753e7b5a3570e7774b79909f864b2846
SaravanaPriya <sarvanspriya@gmail.com>
2023-01-20 10:28:39 +0530

    commit

--- Annotations/Flatten annotation specific to user/Setting Annotation Author Name/src/index.js
+++ Annotations/Flatten annotation specific to user/Setting Annotation Author Name/src/index.js
@@ -1,28 +1,29 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom/client';
-import { PdfViewerComponent, Toolbar, Magnification, Navigation, LinkAnnotation, BookmarkView, 
-         ThumbnailView, Print,TextSelection, Annotation, TextSearch, FormFields, FormDesigner, Inject } from '@syncfusion/ej2-react-pdfviewer';
+import {
+  PdfViewerComponent, Toolbar, Magnification, Navigation, LinkAnnotation, BookmarkView,
+  ThumbnailView, Print, TextSelection, Annotation, TextSearch, FormFields, FormDesigner, Inject} from '@syncfusion/ej2-react-pdfviewer';
 export function App() {
   return (<div>
-    <div className='control-section'>
+    <div className="control-section">
       {/* Render the PDF Viewer */}
       <PdfViewerComponent
-        ref={(scope) => { this.viewer = scope; } }
         id="container"
-        documentPath="PDF Succinctly.pdf"
-        serviceUrl="https://localhost:44399/pdfviewer"
+        documentPath="PDF_Succinctly.pdf"
         documentLoad={documentLoaded}
+        serviceUrl="https://localhost:44399/pdfviewer"
         style={{ height: '640px' }}>
-          
-          {/* Inject the required services */}
-          <Inject services={[Toolbar, Magnification, Navigation, Annotation, LinkAnnotation, BookmarkView, ThumbnailView,
-            Print, TextSelection, TextSearch, FormFields, FormDesigner]} />
-        </PdfViewerComponent>
-      </div>
+
+        {/* Inject the required services */}
+        <Inject services={[Toolbar, Magnification, Navigation, Annotation, LinkAnnotation, BookmarkView, ThumbnailView,
+          Print, TextSelection, TextSearch, FormFields, FormDesigner]} />
+      </PdfViewerComponent>
+    </div>
   </div>);
+  function documentLoaded () {
+      var viewer = document.getElementById('container').ej2_instances[0];
+      viewer.annotationSettings.author = 'syncfusion';
+    };
 }
-function documentLoaded (args) {
-  this.viewer.annotationSettings.author = 'syncfusion';
-};
 const root = ReactDOM.createRoot(document.getElementById('sample'));
 root.render(<App />);
