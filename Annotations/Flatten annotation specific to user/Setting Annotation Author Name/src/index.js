7851391b74213183130458b17edb15cb02d27987
SaravanaPriya <sarvanspriya@gmail.com>
2023-02-02 11:15:17 +0530

    EJ2-68390-sample

--- Annotations/Flatten annotation specific to user/Setting Annotation Author Name/src/index.js
+++ Annotations/Flatten annotation specific to user/Setting Annotation Author Name/src/index.js
@@ -1,45 +1,30 @@
-
-import { createRoot } from 'react-dom/client';
 import * as React from 'react';
-import { PdfViewerComponent, Toolbar, Magnification, Navigation, LinkAnnotation, BookmarkView, ThumbnailView, Print, TextSelection, Annotation, TextSearch, FormFields, FormDesigner, Inject } from '@syncfusion/ej2-react-pdfviewer';
-export class App extends React.Component {
-render() {
-    return (<div>
-        <div className='control-section'>
-        {/* Render the PDF Viewer */}
-        <PdfViewerComponent
-            ref={(scope) => {
-              this.viewer = scope;
-            }}
-            id="container"
-            documentPath="PDF Succinctly.pdf"
-            serviceUrl="https://localhost:44399/pdfviewer"
-            documentLoad={this.documentLoaded}
-            style={{ height: '640px' }}
-          >
-            <Inject
-              services={[
-                Toolbar,
-                Magnification,
-                Navigation,
-                LinkAnnotation,
-                BookmarkView,
-                ThumbnailView,
-                Print,
-                TextSelection,
-                TextSearch,
-                Annotation,
-                FormFields,
-                FormDesigner,
-              ]}
-            />
-          </PdfViewerComponent>
-        </div>
-    </div>);
+import * as ReactDOM from 'react-dom/client';
+import './index.css';
+import {
+  PdfViewerComponent, Toolbar, Magnification, Navigation, LinkAnnotation, BookmarkView,
+  ThumbnailView, Print, TextSelection, Annotation, TextSearch, FormFields, FormDesigner, Inject} from '@syncfusion/ej2-react-pdfviewer';
+export function App() {
+  return (<div>
+    <div className="control-section">
+      {/* Render the PDF Viewer */}
+      <PdfViewerComponent
+        id="container"
+        documentPath="PDF_Succinctly.pdf"
+        documentLoad={documentLoaded}
+        serviceUrl="https://localhost:44309/pdfviewer" 
+        style={{ height: '640px' }}>
+
+        {/* Inject the required services */}
+        <Inject services={[Toolbar, Magnification, Navigation, Annotation, LinkAnnotation, BookmarkView, ThumbnailView,
+          Print, TextSelection, TextSearch, FormFields, FormDesigner]} />
+      </PdfViewerComponent>
+    </div>
+  </div>);
+  function documentLoaded () {
+      var viewer = document.getElementById('container').ej2_instances[0];
+      viewer.annotationSettings.author = 'syncfusion';
+    };
 }
-documentLoaded = (args) => {
-  this.viewer.annotationSettings.author = 'syncfusion';
-};
-}
-const root = createRoot(document.getElementById('sample'));
+const root = ReactDOM.createRoot(document.getElementById('sample'));
 root.render(<App />);
