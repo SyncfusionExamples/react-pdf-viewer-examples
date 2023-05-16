d830539f81d11cbf0bf494d02a472df5baf59bfa
SaravanaPriya31 <sarvanspriya@gmail.com>
2023-05-16 10:26:39 +0530

    commit

--- Annotations/Flatten all annotations/Web Service/Controllers/PdfViewerController.cs
+++ Annotations/Flatten all annotations/Web Service/Controllers/PdfViewerController.cs
@@ -26,7 +26,7 @@ namespace PDFViewerWebService.Controllers
         }
 
         [HttpPost("Load")]
-/*        [Microsoft.AspNetCore.Cors.EnableCors("MyPolicy")]*/
+        [Microsoft.AspNetCore.Cors.EnableCors("MyPolicy")]
         [Route("[controller]/Load")]
         //Post action for loading the PDF documents 
         public IActionResult Load([FromBody] Dictionary<string, string> jsonObject)
@@ -65,7 +65,7 @@ namespace PDFViewerWebService.Controllers
 
         [AcceptVerbs("Post")]
         [HttpPost("Bookmarks")]
-/*        [Microsoft.AspNetCore.Cors.EnableCors("MyPolicy")]*/
+        [Microsoft.AspNetCore.Cors.EnableCors("MyPolicy")]
         [Route("[controller]/Bookmarks")]
         //Post action for processing the bookmarks from the PDF documents
         public IActionResult Bookmarks([FromBody] Dictionary<string, string> jsonObject)
@@ -78,7 +78,7 @@ namespace PDFViewerWebService.Controllers
 
         [AcceptVerbs("Post")]
         [HttpPost("RenderPdfPages")]
-/*        [Microsoft.AspNetCore.Cors.EnableCors("MyPolicy")]*/
+        [Microsoft.AspNetCore.Cors.EnableCors("MyPolicy")]
         [Route("[controller]/RenderPdfPages")]
         //Post action for processing the PDF documents  
         public IActionResult RenderPdfPages([FromBody] Dictionary<string, string> jsonObject)
@@ -91,7 +91,7 @@ namespace PDFViewerWebService.Controllers
 
         [AcceptVerbs("Post")]
         [HttpPost("RenderPdfTexts")]
-/*        [Microsoft.AspNetCore.Cors.EnableCors("MyPolicy")]*/
+        [Microsoft.AspNetCore.Cors.EnableCors("MyPolicy")]
         [Route("[controller]/RenderPdfTexts")]
         //Post action for processing the PDF texts  
         public IActionResult RenderPdfTexts([FromBody] Dictionary<string, string> jsonObject)
@@ -104,7 +104,7 @@ namespace PDFViewerWebService.Controllers
 
         [AcceptVerbs("Post")]
         [HttpPost("RenderThumbnailImages")]
-/*        [Microsoft.AspNetCore.Cors.EnableCors("MyPolicy")]*/
+        [Microsoft.AspNetCore.Cors.EnableCors("MyPolicy")]
         [Route("[controller]/RenderThumbnailImages")]
         //Post action for rendering the thumbnail images
         public IActionResult RenderThumbnailImages([FromBody] Dictionary<string, string> jsonObject)
@@ -117,7 +117,7 @@ namespace PDFViewerWebService.Controllers
 
         [AcceptVerbs("Post")]
         [HttpPost("RenderAnnotationComments")]
-/*        [Microsoft.AspNetCore.Cors.EnableCors("MyPolicy")]*/
+        [Microsoft.AspNetCore.Cors.EnableCors("MyPolicy")]
         [Route("[controller]/RenderAnnotationComments")]
         //Post action for rendering the annotations
         public IActionResult RenderAnnotationComments([FromBody] Dictionary<string, string> jsonObject)
@@ -130,7 +130,7 @@ namespace PDFViewerWebService.Controllers
 
         [AcceptVerbs("Post")]
         [HttpPost("ExportAnnotations")]
-/*        [Microsoft.AspNetCore.Cors.EnableCors("MyPolicy")]*/
+        [Microsoft.AspNetCore.Cors.EnableCors("MyPolicy")]
         [Route("[controller]/ExportAnnotations")]
         //Post action to export annotations
         public IActionResult ExportAnnotations([FromBody] Dictionary<string, string> jsonObject)
@@ -141,7 +141,7 @@ namespace PDFViewerWebService.Controllers
         }
         [AcceptVerbs("Post")]
         [HttpPost("ImportAnnotations")]
-/*        [Microsoft.AspNetCore.Cors.EnableCors("MyPolicy")]*/
+        [Microsoft.AspNetCore.Cors.EnableCors("MyPolicy")]
         [Route("[controller]/ImportAnnotations")]
         //Post action to import annotations
         public IActionResult ImportAnnotations([FromBody] Dictionary<string, string> jsonObject)
@@ -189,8 +189,8 @@ namespace PDFViewerWebService.Controllers
         }
 
         [AcceptVerbs("Post")]
-        [HttpPost("Unload")]
-/*        [Microsoft.AspNetCore.Cors.EnableCors("MyPolicy")]*/
+        [HttpPost("ExportFormFields")]
+        [Microsoft.AspNetCore.Cors.EnableCors("MyPolicy")]
         [Route("[controller]/ExportFormFields")]
         //Post action to export form fields
         public IActionResult ExportFormFields([FromBody] Dictionary<string, string> jsonObject)
@@ -202,8 +202,8 @@ namespace PDFViewerWebService.Controllers
         }
 
         [AcceptVerbs("Post")]
-        [HttpPost("Unload")]
-/*        [Microsoft.AspNetCore.Cors.EnableCors("MyPolicy")]*/
+        [HttpPost("ImportFormFields")]
+        [Microsoft.AspNetCore.Cors.EnableCors("MyPolicy")]
         [Route("[controller]/ImportFormFields")]
         //Post action to import form fields
         public IActionResult ImportFormFields([FromBody] Dictionary<string, string> jsonObject)
@@ -216,7 +216,7 @@ namespace PDFViewerWebService.Controllers
 
         [AcceptVerbs("Post")]
         [HttpPost("Unload")]
-/*        [Microsoft.AspNetCore.Cors.EnableCors("MyPolicy")]*/
+        [Microsoft.AspNetCore.Cors.EnableCors("MyPolicy")]
         [Route("[controller]/Unload")]
         //Post action for unloading and disposing the PDF document resources  
         public IActionResult Unload([FromBody] Dictionary<string, string> jsonObject)
@@ -229,7 +229,7 @@ namespace PDFViewerWebService.Controllers
 
 
         [HttpPost("Download")]
-/*        [Microsoft.AspNetCore.Cors.EnableCors("MyPolicy")]*/
+        [Microsoft.AspNetCore.Cors.EnableCors("MyPolicy")]
         [Route("[controller]/Download")]
         //Post action for downloading the PDF documents
         public IActionResult Download([FromBody] Dictionary<string, string> jsonObject)
