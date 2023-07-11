377a240065f8f336bf58714d6ed62a375df32a7a
SaravanaPriya31 <Saravana31>
2023-07-11 23:42:39 +0530

    837867-azure

---
+++ Save and Load/Open-Save-PDF-document-Azure-Blob-Storage/Open-Save-PDF-document-Azure-Blob-Storage/src/index.js
@@ -0,0 +1,23 @@
+import * as ReactDOM from 'react-dom/client';
+import * as React from 'react';
+import './index.css';
+import { PdfViewerComponent, Toolbar, Magnification, Navigation, LinkAnnotation, BookmarkView,
+         ThumbnailView, Print, TextSelection, Annotation, TextSearch, FormFields, FormDesigner, Inject} from '@syncfusion/ej2-react-pdfviewer';
+export function App() {
+return (<div>
+    <div className='control-section'>
+        <PdfViewerComponent 
+            id="container" 
+            documentPath="PDF_Succinctly.pdf" 
+            // Replace the "localhost:44396" with the actual URL of your server
+            serviceUrl="https://localhost:44396/pdfviewer" 
+            style={{ 'height': '640px' }}>
+               {/* Inject the required services */}
+               <Inject services={[ Toolbar, Magnification, Navigation, Annotation, LinkAnnotation, BookmarkView, ThumbnailView,
+                                   Print, TextSelection, TextSearch, FormFields, FormDesigner]} />
+        </PdfViewerComponent>
+    </div>
+</div>);
+}
+const root = ReactDOM.createRoot(document.getElementById('sample'));
+root.render(<App />);
