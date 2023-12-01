c62d6f831f667b92e152e15f5667c9bc35b08409
SaravanaPriya31 <Saravana31>
2023-12-01 14:18:39 +0530

    commit

--- How to/Get hash id from storage/src/index.js
+++ How to/Get hash id from storage/src/index.js
@@ -1,55 +1,32 @@
+import * as ReactDOM from 'react-dom';
 import * as React from 'react';
-import { createRoot } from 'react-dom/client';
-import { 
-    PdfViewerComponent, 
-    Toolbar, 
-    Magnification, 
-    Navigation, 
-    LinkAnnotation, 
-    BookmarkView, 
-    ThumbnailView, 
-    Print, 
-    TextSelection, 
-    Annotation, 
-    TextSearch, 
-    FormFields, 
-    FormDesigner, 
-    Inject } from '@syncfusion/ej2-react-pdfviewer';
-export class App extends React.Component{
-      render() {
-        return (
-          <div>
-            <div className="control-section">
-              {/* Render the PDF Viewer */}
-              <PdfViewerComponent
-                id="container"
-                documentPath="https://cdn.syncfusion.com/content/pdf/pdf-succinctly.pdf"    
-                //To set up the **server-backed PDF Viewer**, add the following 'serviceUrl'.           
-                // serviceUrl="https://services.syncfusion.com/react/production/api/pdfviewer"
-                style={{ height: '640px' }}
-              >
-                {/* Inject the required services */}  
-                <Inject
-                  services={[
-                    Toolbar,
-                    Magnification,
-                    Navigation,
-                    LinkAnnotation,
-                    BookmarkView,
-                    ThumbnailView,
-                    Print,
-                    TextSelection,
-                    TextSearch,
-                    Annotation,
-                    FormFields,
-                    FormDesigner,
-                  ]}
-                />
-              </PdfViewerComponent>
-            </div>
-          </div>
-        );
-      }
+import { PdfViewerComponent, Toolbar, Magnification, Navigation, LinkAnnotation, BookmarkView,
+         ThumbnailView, Print, TextSelection, Annotation, TextSearch, Inject } from '@syncfusion/ej2-react-pdfviewer';
+
+function App() {
+  function uniqueId() {
+    var viewer = document.getElementById('container').ej2_instances[0];	
+    var uniqueId = viewer.viewerBase.documentId
+    var hashId = window.sessionStorage.getItem(uniqueId+'_hashId');
+    //Prints the id in the console window.
+    console.log(hashId);
+  }
+  return (<div>
+    <div className='control-section'>
+     {/* Render the PDF Viewer */}
+     <button onClick={uniqueId}>unique Id</button>
+      <PdfViewerComponent
+        id="container"
+        documentPath="https://cdn.syncfusion.com/content/pdf/pdf-succinctly.pdf"
+        serviceUrl="https://services.syncfusion.com/react/production/api/pdfviewer"
+        height= '600px'>
+              <Inject services={[ Toolbar, Magnification, Navigation, LinkAnnotation, Annotation, 
+                                  BookmarkView, ThumbnailView, Print, TextSelection, TextSearch]} />
+      </PdfViewerComponent>
+    </div>
+  </div>
+  );
 }
-const root = createRoot(document.getElementById('sample'));
+const root = ReactDOM.createRoot(document.getElementById('sample'));
 root.render(<App />);
+
