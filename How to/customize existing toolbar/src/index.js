9c589df71211a499ede2740f104645e107af27d2
SaravanaPriya31 <Saravana31>
2024-01-02 17:49:23 +0530

    856503 : Customize existing toolbar in PDF Viewer component sample update

--- How to/customize existing toolbar/src/index.js
+++ How to/customize existing toolbar/src/index.js
@@ -3,13 +3,44 @@ import * as React from 'react';
 import './index.css';
 import  { PdfViewerComponent, Toolbar, Magnification, Navigation, LinkAnnotation, BookmarkView,
          ThumbnailView, Print, TextSelection, Annotation, TextSearch, FormFields, FormDesigner, Inject} from '@syncfusion/ej2-react-pdfviewer';
+import { ComboBox } from "@syncfusion/ej2-dropdowns";
+import { TextBox } from "@syncfusion/ej2-inputs";           
+
 export function App() {
-  var toolItem = { 
-    prefixIcon :'e-icons e-paste',
-    id :'print',
-    tooltipText : 'Custom toolbar item',
-    align : 'left',
-  }
+
+// Add OnCreateSearch outside the App function
+function OnCreateSearch() {
+  this.addIcon('prepend', 'e-icons e-search');
+}
+
+  var toolItem1 = {
+    prefixIcon: 'e-icons e-paste',
+    id: 'print',
+    tooltipText: 'Custom toolbar item',
+};
+var toolItem2  = {
+    id: 'download',
+    text: 'Save',
+    tooltipText: 'Custom toolbar item',
+    align: 'right'
+};
+var LanguageList = ['Typescript', 'Javascript', 'Angular', 'C#', 'C', 'Python'];
+var toolItem3 = {
+    type: 'Input',
+    tooltipText: 'Language List',
+    cssClass: 'percentage',
+    align: 'Left',
+    id: 'dropdown',
+    template: new ComboBox({ width: 100, value: 'TypeScript', dataSource: LanguageList, popupWidth: 85, showClearButton: false, readonly: false })  
+};
+var toolItem4 = {
+    type: 'Input',
+    tooltipText: 'Text',
+    align: 'Right',
+    cssClass: 'find',
+    id: 'textbox',
+    template: new TextBox({ width: 125, placeholder: 'Type Here', created: OnCreateSearch})
+}
   function toolbarClick(args){
     var viewer = document.getElementById('container').ej2_instances[0];
     if (args.item && args.item.id === 'print') {
@@ -21,13 +52,11 @@ export function App() {
   };
 return (<div>
     <div className='control-section'>
-        //To set up the **server-backed PDF Viewer**, add the following 'serviceUrl'.
-        //serviceUrl="https://services.syncfusion.com/react/production/api/pdfviewer"
         <PdfViewerComponent 
             id="container" 
             documentPath="https://cdn.syncfusion.com/content/pdf/pdf-succinctly.pdf"
             resourceUrl="https://cdn.syncfusion.com/ej2/24.1.41/dist/ej2-pdfviewer-lib"
-            toolbarSettings={{ showTooltip : true, toolbarItems: [toolItem, 'OpenOption', 'PageNavigationTool', 'MagnificationTool', 'PanTool', 'SelectionTool', 'SearchOption', 'PrintOption', 'DownloadOption', 'UndoRedoTool', 'AnnotationEditTool', 'FormDesignerEditTool', 'CommentTool', 'SubmitForm']}}
+            toolbarSettings={{ showTooltip : true, toolbarItems: [toolItem1, toolItem2, 'OpenOption', 'PageNavigationTool', 'MagnificationTool', toolItem3, 'PanTool', 'SelectionTool', 'SearchOption', 'PrintOption', 'DownloadOption', 'UndoRedoTool', 'AnnotationEditTool', 'FormDesignerEditTool', toolItem4, 'CommentTool', 'SubmitForm']}}
             toolbarClick={toolbarClick}
             style={{ 'height': '640px' }}>
                {/* Inject the required services */}
