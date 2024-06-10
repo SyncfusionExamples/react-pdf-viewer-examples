f2e60f211e7dd774080abf9e6dcab95c1d98f4ce
SaravanaPriya31 <Saravana31>
2024-06-10 17:20:13 +0530

    889569: commit

---
+++ How to/Download Start event/src/index.js
@@ -0,0 +1,31 @@
+import * as ReactDOM from 'react-dom';
+import * as React from 'react';
+import './index.css';
+import { PdfViewerComponent, Toolbar, Magnification, Navigation, LinkAnnotation, BookmarkView,
+         ThumbnailView, Print, TextSelection, Annotation, TextSearch,PageOrganizer, Inject } from '@syncfusion/ej2-react-pdfviewer';
+let pdfviewer;
+
+function App() {
+  function downloadStart(args){
+    // Your custom logic here
+    args.cancel = true; // Prevent download action
+};
+  return (<div>
+    <div className='control-section'>
+     {/* Render the PDF Viewer */}
+      <PdfViewerComponent
+        ref={(scope) => { pdfviewer = scope; }}
+        id="container"
+        documentPath="https://cdn.syncfusion.com/content/pdf/pdf-succinctly.pdf"
+        resourceUrl="https://cdn.syncfusion.com/ej2/24.1.41/dist/ej2-pdfviewer-lib"
+        downloadStart={downloadStart}
+        style={{ 'height': '640px' }}>
+              <Inject services={[ Toolbar, Magnification, Navigation, LinkAnnotation, Annotation, 
+                                  BookmarkView, ThumbnailView, Print, TextSelection, TextSearch,PageOrganizer]} />
+      </PdfViewerComponent>
+    </div>
+  </div>
+  );
+}
+const root = ReactDOM.createRoot(document.getElementById('sample'));
+root.render(<App />);
--- How to/Restrict Zoom Percentage on Mobile Devices/src/index.js
+++ How to/Restrict Zoom Percentage on Mobile Devices/src/index.js
@@ -1,40 +1,27 @@
 import * as ReactDOM from 'react-dom';
 import * as React from 'react';
 import './index.css';
+import {Browser} from '@syncfusion/ej2-base';
 import { PdfViewerComponent, Toolbar, Magnification, Navigation, LinkAnnotation, BookmarkView, ThumbnailView, Print, TextSelection, Annotation, TextSearch, FormFields, FormDesigner, PageOrganizer, Inject } from '@syncfusion/ej2-react-pdfviewer';
 export function App() {
     var isInitialLoading = true;
     function documentLoaded() {
         var viewer = document.getElementById('container').ej2_instances[0];
-        if (isInitialLoading) {
-            viewer.isPageOrganizerOpen = true;
-            isInitialLoading = false;
-        } else {
-            viewer.isPageOrganizerOpen = false;
+        if (Browser.isDevice && !viewer.enableDesktopMode) {
+            viewer.maxZoom = 200;
+            viewer.minZoom = 10;
+        }
+        else{
+            viewer.zoomMode = 'Default';
         }
     }
-    function openPageOrganizer() {
-        var viewer = document.getElementById('container').ej2_instances[0];
-        // Open PageOrganizer Pane
-        viewer.pageOrganizer.openPageOrganizer();
-    }
-    function closePageOrganizer() {
-        var viewer = document.getElementById('container').ej2_instances[0];
-        // Close Page Organizer Pane
-        viewer.pageOrganizer.closePageOrganizer();
-     }
 return (<div>
     <div className='control-section'>
-    <button onClick={openPageOrganizer}>Open PageOrganizer Pane</button>
-    <button onClick={closePageOrganizer}>Close PageOrganizer Pane</button>
         <PdfViewerComponent 
             id="container" 
             documentPath="PDF_Succinctly.pdf" 
             serviceUrl="https://ej2services.syncfusion.com/production/web-services/api/pdfviewer" 
             documentLoad = {documentLoaded} 
-            isPageOrganizerOpen={false}
-            enablePageOrganizer={true}
-            pageOrganizerSettings = {{canDelete: true, canInsert: true, canRotate: true, canCopy: true, canRearrange: true}}
             style={{ 'height': '640px' }}>
 
             <Inject services={[Toolbar, Magnification, Navigation, Annotation, LinkAnnotation, BookmarkView, ThumbnailView, Print, TextSelection, TextSearch, FormFields, FormDesigner, PageOrganizer]}/>
