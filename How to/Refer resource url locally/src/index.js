7999d7cb320d39fbda8f99a5a6dc034e8c86aa6d
Uma-SF4267 <umamageshwari.shanmugam@syncfusion.com>
2025-02-05 13:08:18 +0530

    931453: How to reference resource URL locally in the PDF Viewer without using CDN links?

---
+++ How to/Refer resource url locally/src/index.js
@@ -0,0 +1,20 @@
+import * as ReactDOM from 'react-dom';
+import * as React from 'react';
+import './index.css';
+import { PdfViewerComponent, Toolbar, Magnification, Navigation, LinkAnnotation, BookmarkView, ThumbnailView, Print, TextSelection, Annotation, TextSearch, FormFields, FormDesigner, PageOrganizer, Inject } from '@syncfusion/ej2-react-pdfviewer';
+export function App() {
+return (<div>
+    <div className='control-section'>
+        <PdfViewerComponent 
+            id="container" 
+            documentPath= {window.location.origin + "/assest/pdfsuccinctly.pdf"} 
+            resourceUrl = {window.location.origin + "/assest/ej2-pdfviewer-lib"} 
+            style={{ 'height': '640px' }}>
+
+            <Inject services={[Toolbar, Magnification, Navigation, Annotation, LinkAnnotation, BookmarkView, ThumbnailView, Print, TextSelection, TextSearch, FormFields, FormDesigner, PageOrganizer]}/>
+        </PdfViewerComponent>
+    </div>
+</div>);
+}
+const root = ReactDOM.createRoot(document.getElementById('sample'));
+root.render(<App />);
