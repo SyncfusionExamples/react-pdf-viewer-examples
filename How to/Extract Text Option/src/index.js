fc943f38bf756ef9a6ddef78d89d8682c908df25
SF4524LogeshKumar <logeshkumar.dhamodharan@syncfusion.com>
2025-03-27 12:27:38 +0530

    948910: Updated sample for Extract Text Option

---
+++ How to/Extract Text Option/src/index.js
@@ -0,0 +1,22 @@
+import * as ReactDOM from 'react-dom';
+import * as React from 'react';
+import './index.css';
+import { PdfViewerComponent, Toolbar, Magnification, Navigation, LinkAnnotation, BookmarkView, ThumbnailView, Print, TextSelection, Annotation, TextSearch, FormFields, FormDesigner, Inject } from '@syncfusion/ej2-react-pdfviewer';
+export function App() {
+  return (<div>
+    <div className='control-section'>
+      <PdfViewerComponent 
+        id="container" 
+        documentPath="https://cdn.syncfusion.com/content/pdf/pdf-succinctly.pdf"
+        resourceUrl="https://cdn.syncfusion.com/ej2/29.1.33/dist/ej2-pdfviewer-lib"
+        extractTextOption='None'  // Options: 'None', 'TextOnly', 'BoundsOnly', 'TextAndBounds' 
+        style={{ 'height': '680px' }} 
+      >
+        <Inject services={[Toolbar, Magnification, Navigation, Annotation, LinkAnnotation, BookmarkView, ThumbnailView,
+          Print, TextSelection, TextSearch, FormFields, FormDesigner]} />
+      </PdfViewerComponent>
+    </div>
+  </div>);
+}
+const root = ReactDOM.createRoot(document.getElementById('sample'));
+root.render(<App />);
